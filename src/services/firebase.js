1634b71791c4cc4bd0fe080abb1c363b2cdb558f
Rishi <rishi.srighakollapu@gmail.com>
2026-01-07 11:47:10 +0530

    added databse code

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,13 +2,27 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-app.firebaseapp.com",
-  projectId: "your-app-id",
-  storageBucket: "your-app.appspot.com",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyAq5ZhKNEo6XWqwQm-a1gsmctj0jvw_8Tc",
+  authDomain: "foodlink-699ab.firebaseapp.com",
+  projectId: "foodlink-699ab",
+  storageBucket: "foodlink-699ab.firebasestorage.app",
+  messagingSenderId: "750513582982",
+  appId: "1:750513582982:web:ac075d21b18e9e67bbf207",
+  measurementId: "G-CQ09RC6YYV"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+async function createTestDocument() {
+  try {
+    await setDoc(doc(db, "users", "testUser"), {
+      name: "Test Name",
+      role: "Admin"
+    });
+    console.log("Document written! Check the Firestore Console.");
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+createTestDocument();
